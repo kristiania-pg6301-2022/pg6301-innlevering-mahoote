5afed69d9dbb09185e01ed4ac6378b77e685f0ea
Martin Teigen <martin@teigen.biz>
2022-02-25 14:08:19 +0100

    Created front page

--- client/index.jsx
+++ client/index.jsx
@@ -1,4 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-ReactDOM.render(<h1>Hello Quiz</h1>, document.getElementById("app"));
+function FrontPage() {
+  return <div>Front page</div>;
+}
+
+function Application() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<FrontPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.render(<Application />, document.getElementById("app"));
