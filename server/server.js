007e9d720da680f5cd84ab308adb7375391e20b9
Martin Teigen <martin@teigen.biz>
2022-03-03 17:33:03 +0100

    Fake cookies

--- server/server.js
+++ server/server.js
@@ -4,27 +4,38 @@ import * as path from "path";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
-app.use((req, res, next) => {
-  const answers = req.signedCookies;
-  console.log(answers);
-  next();
-});
+let totalAnswers = 0;
+let correctAnswers = 0;
 
 app.get("/api/question", (req, res) => {
   const question = randomQuestion();
   res.json(question);
 });
 
+app.get("/api/results", (req, res) => {
+  const cookies = req.signedCookies;
+  res.json({ tot: cookies["totalAnswers"], cor: cookies["correctAnswers"] });
+});
+
 app.post("/api/question", (req, res) => {
   const { question, userAnswer } = req.body;
   const isCorrect = isCorrectAnswer(question, userAnswer);
-  res.cookie("question-" + question.id, isCorrect, { signed: true });
+
+  if (isCorrect)
+    res.cookie("correctAnswers", correctAnswers++, { signed: true });
+  res.cookie("totalAnswers", totalAnswers++, { signed: true });
+
+  res.sendStatus(200);
+});
+
+app.get("/api/deleteCookies", (req, res) => {
   res.sendStatus(200);
 });
 
