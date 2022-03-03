26352afb2621bb420e336dcca55b61b4a1d34ffb
Jonasthj <jonasthenderupjensen@gmail.com>
2022-03-03 15:45:38 +0100

    /question reloads after post request

--- client/index.jsx
+++ client/index.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import ReactDOM from "react-dom";
+import ReactDOM, { render } from "react-dom";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
 function FrontPage() {
@@ -13,6 +13,7 @@ function FrontPage() {
 
 function Question() {
   const [question, setQuestion] = useState();
+
   useEffect(async () => {
     const res = await fetch("/api/question");
     setQuestion(await res.json());
@@ -20,18 +21,17 @@ function Question() {
 
   if (!question) return <div>Loading...</div>;
 
-  function handleConfirm(question, userAnswer) {
+  function handleNext(question, userAnswer) {
     fetch("/api/question", {
       method: "post",
       headers: {
         "content-type": "application/json",
       },
       body: JSON.stringify({ question, userAnswer }),
+    }).then(async () => {
+      const temp = await fetch("/api/question");
+      setQuestion(await temp.json());
     });
-    // .then((response) => response.json())
-    // .then((status) => {
-    //   console.log(status.answer);
-    // });
   }
 
   return (
@@ -41,11 +41,12 @@ function Question() {
         .filter((a) => question.answers[a])
         .map((answer) => (
           <div key={answer}>
-            <button onClick={(q) => handleConfirm(question, answer)}>
+            <button onClick={(q) => handleNext(question, answer)}>
               {question.answers[answer]}
             </button>
           </div>
         ))}
+      <button>Submit Questionnaire</button>
     </div>
   );
 }
